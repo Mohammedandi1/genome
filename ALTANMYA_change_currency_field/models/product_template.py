068c9a434fbba73f76f13580a026d69c377386d4
aalgenaimi <136240554+aalgenaimi@users.noreply.github.com>
2023-11-04 17:21:31 +0300

    Update product_template.py

--- ALTANMYA_change_currency_field/models/product_template.py
+++ ALTANMYA_change_currency_field/models/product_template.py
@@ -6,7 +6,8 @@ class ProductTemplate(models.Model):
     currency_id = fields.Many2one(
         comodel_name='res.currency',
         string='Currency',
-        related=False,  # You can customize this based on your needs.
-        store=True,  # This makes the field stored.
-        readonly=False,  # You can make it read-only if needed.
+        related=False,
+        store=True,
+        readonly=False,
+        required=True,
     )
