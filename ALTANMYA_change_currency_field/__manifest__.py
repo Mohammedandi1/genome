087a88d7404a29a35ceb7cd21490a768a389a9b3
aalgenaimi <136240554+aalgenaimi@users.noreply.github.com>
2023-11-04 16:58:05 +0300

    Add files via upload

---
+++ ALTANMYA_change_currency_field/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "ALTANMYA Change Currency Field",
+    'summary': "Change currency_id field to be stored in product.template",
+    'version': '1.0',
+    'category': 'Custom Development',
+    'author': "ALTANMYA",
+    'website': "https://www.tech.altanmya.net",
+    'depends': ['product'],
+    'data': [ ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
