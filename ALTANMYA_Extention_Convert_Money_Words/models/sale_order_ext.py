ee25044cf942eb2f5b8607f446daee5a2f753709
aalgenaimi <136240554+aalgenaimi@users.noreply.github.com>
2025-02-09 14:40:56 +0300

    Add files via upload

--- ALTANMYA_Extention_Convert_Money_Words/models/sale_order_ext.py
+++ ALTANMYA_Extention_Convert_Money_Words/models/sale_order_ext.py
@@ -66,6 +66,9 @@ class SaleOrderSpelling(models.Model):
             if self.currency_id.id == 136:
                 ar_currency_unit = 'ليرة سورية'
                 ar_currency_subunit = 'قرش'
+            elif self.currency_id.id == 90:
+                ar_currency_unit = 'دينار أردني'
+                ar_currency_subunit = 'قرش'   
             self.spelling_total_ar = self.get_spelling_num(total, lang='ar',
                                                             currency_unit=ar_currency_unit,
                                                             currency_subunit=ar_currency_subunit)
