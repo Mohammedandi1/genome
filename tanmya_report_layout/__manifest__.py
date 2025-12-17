2e143de7ef360415698c57a7a23ea4b2438a11f8
krmo-odoo <krmo@odoo.com>
2025-12-17 19:10:58 +0530

    [ADD] added modules

--- tanmya_report_layout/__manifest__.py
+++ tanmya_report_layout/__manifest__.py
@@ -1,24 +1,23 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'ALTANMYA Report Layout',
-    'version': '1.0',
-    'category': 'Web/Reporting',
-    'summary': 'Define a custom report layout',
-    'depends': ['web', 'web_studio'],
-    'author': 'ALTANMYA - TECHNOLOGY SOLUTIONS',
-    'company': 'ALTANMYA - TECHNOLOGY SOLUTIONS Part of ALTANMYA GROUP',
-    'website': "http://tech.altanmya.net",
-    'data': [
-         'views/tanmya_layout.xml',
+    "name": "ALTANMYA Report Layout",
+    "version": "18.0.1.0.0",
+    "category": "Web/Reporting",
+    "summary": "Define a custom report layout",
+    "depends": ["web", "web_studio"],
+    "author": "ALTANMYA - TECHNOLOGY SOLUTIONS",
+    "company": "ALTANMYA - TECHNOLOGY SOLUTIONS Part of ALTANMYA GROUP",
+    "website": "http://tech.altanmya.net",
+    "data": [
+        "views/tanmya_layout.xml",
     ],
-    'assets': {
-        'web_studio.studio_assets': [
-            'tanmya_report_layout/static/src/js/tanmya_layout.js'
-        ]
+    "assets": {
+        "web_studio.studio_assets_minimal": [
+            "tanmya_report_layout/static/src/js/tanmya_layout.js",
+        ],
     },
-    'installable': True,
-    'auto_install': False,
-    'application': False,
-    'license': 'LGPL-3',
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "license": "LGPL-3",
 }
